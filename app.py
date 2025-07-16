24d424f1db83bf058730600c6c8b78fbc2819715
judirispah <judi.rispah.0606@gmail.com>
2025-07-17 00:57:53 +0530

    place search tool

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Main heading
+st.title("🌍 AI Travel Agent & Expense Planner")
+
+# Subheading / Tagline
+st.subheader("Plan your trip with real-time data, personalized itinerary, and total expense breakdown.")
