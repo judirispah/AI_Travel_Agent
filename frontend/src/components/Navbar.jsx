d8879f2afeee3c928d5c549915c9ce00d74497ff
judirispah <judi.rispah.0606@gmail.com>
2025-07-21 22:23:15 +0530

    AI-6 <message>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Nav({ setActiveTab }) {
       <ul className="flex space-x-6 text-gray-600 font-medium">
         <li onClick={() => setActiveTab("itinerary")} className="hover:text-indigo-500 cursor-pointer">Itinerary</li>
         <li onClick={() => setActiveTab("flights")} className="hover:text-indigo-500 cursor-pointer">Flights</li>
-        <li onClick={() => setActiveTab("hotels")} className="hover:text-indigo-500 cursor-pointer">Hotels</li>
+        <li onClick={() => setActiveTab("hotel")} className="hover:text-indigo-500 cursor-pointer">Hotels</li>
         <li onClick={() => setActiveTab("map")} className="hover:text-indigo-500 cursor-pointer">Map View</li>
       </ul>
     </nav>
